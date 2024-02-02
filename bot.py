2b219cb3466d09a02f35cad55dc4b778052bfdd3
Tim Raaymakers <43066279+timraay@users.noreply.github.com>
2024-02-02 11:36:59 +0100

    Make Discord verification compliant

--- bot.py
+++ bot.py
@@ -10,10 +10,6 @@ from pathlib import Path
 from lib.hss.api import HSSApi
 from utils import get_config, ttl_cache
 
-BOT_COMMAND_PREFIX = get_config()['Bot']['CommandPrefix']
-BOT_UNLOCK_DEVELOPER_COMMANDS = get_config().getboolean('Bot', 'UnlockDeveloperCommands')
-BOT_INTENTS = discord.Intents.all() if BOT_UNLOCK_DEVELOPER_COMMANDS else discord.Intents.default()
-
 HSS_API_BASE = get_config().get('HSS', 'ApiBaseUrl')
 
 async def load_all_cogs():
@@ -55,45 +51,40 @@ class Bot(commands.Bot):
         return await self.hss.teams()
 
 bot = Bot(
-    intents=BOT_INTENTS,
-    command_prefix=BOT_COMMAND_PREFIX,
+    intents=discord.Intents.default(),
+    command_prefix=commands.when_mentioned,
     case_insensitive=True
 )
 
 
-@bot.group(invoke_without_command=True, aliases=['cog'])
+@bot.command(aliases=["load"])
 @commands.is_owner()
-async def module(ctx):
-    await ctx.send(f"**Available Operations**\n{ctx.prefix}cog reload [cog]\n{ctx.prefix}cog enable <cog>\n{ctx.prefix}cog disable <cog>\n{ctx.prefix}cog info <cog>")
-
-@module.command(aliases=["load"])
-@commands.is_owner()
-async def enable(ctx, cog: str):
+async def enable(ctx: commands.Context, cog: str):
     """ Enable a cog """
     cog = cog.lower()
-    if os.path.exists(Path(f"./cogs/{cog}.py")):
+    if Path(f"./cogs/{cog}.py").exists():
         await bot.load_extension(f"cogs.{cog}")
         await ctx.send(f"Enabled {cog}")
     else:
         await ctx.send(f"{cog} doesn't exist")
 
-@module.command(aliases=["unload"])
+@bot.command(aliases=["unload"])
 @commands.is_owner()
-async def disable(ctx, cog: str):
+async def disable(ctx: commands.Context, cog: str):
     """ Disable a cog """
     cog = cog.lower()
-    if os.path.exists(Path(f"./cogs/{cog}.py")):
+    if Path(f"./cogs/{cog}.py").exists():
         await bot.unload_extension(f"cogs.{cog}")
         await ctx.send(f"Disabled {cog}")
     else:
         await ctx.send(f"{cog} doesn't exist")
 
-@module.command()
+@bot.command()
 @commands.is_owner()
-async def reload(ctx, cog: str = None):
+async def reload(ctx: commands.Context, cog: str = None):
     """ Reload cogs """
 
-    async def reload_cog(ctx, cog):
+    async def reload_cog(ctx: commands.Context, cog: str):
         """ Reloads a cog """
         try:
             await bot.reload_extension(f"cogs.{cog}")
@@ -107,54 +98,9 @@ async def reload(ctx, cog: str = None):
                 cog = cog.replace(".py", "")
                 await reload_cog(ctx, cog)
     else:
-        if os.path.exists(Path(f"./cogs/{cog}.py")):
+        if Path(f"./cogs/{cog}.py").exists():
             await reload_cog(ctx, cog)
         else:
             await ctx.send(f"{cog} doesn't exist")
-
-@module.command(aliases=["list"])
-@commands.is_owner()
-async def info(ctx, cog: str = None):
-    """ List cogs' commands and events """
-
-    if cog:
-        # A single cog
-        cog = cog.lower()
-        if os.path.exists(Path(f"./cogs/{cog}.py")):
-            cog = bot.get_cog(cog)
-            if not cog:
-                await ctx.send(f"{cog} is not a module")
-            else:
-                commands_list = [command.name for command in cog.get_commands()]
-                events_list = [listener.name for listener in cog.get_listeners()]
-
-                if not commands_list: commands = "None"
-                else: commands = ", ".join(commands_list)
-
-                if not events_list: events = "None"
-                else: events = ", ".join(events_list)
-
-                embed=discord.Embed(title=f"Module \"{cog.qualified_name}\"")
-                embed.add_field(name=f"Commands ({str(len(commands_list))})", value=commands, inline=False)
-                embed.add_field(name=f"Events ({str(len(events_list))})", value=events, inline=False)
-
-                await ctx.send(embed=embed)
-        else:
-            await ctx.send(f"{cog} doesn't exist")
-    
-    else:
-        # All cogs
-        embed = discord.Embed(title="All modules")
-        for cog in os.listdir(Path("./cogs")):
-            if cog.endswith(".py"):
-                cog = cog.replace(".py", "")
-                cog = bot.get_cog(cog)
-                if cog:
-                    commands_list = cog.get_commands()
-                    events_list = cog.get_listeners()
-
-                    embed.add_field(name=cog.qualified_name, value=f"{str(len(commands_list))} commands & {str(len(events_list))} events", inline=False)
-        await ctx.send(embed=embed)
-
 
 bot.run(get_config()['Bot']['Token'])
